4e5a3daeeb6d1b5d81685e860d164c09d206e8aa
Rolivhuwa-Shally02 <80606439+Rolivhuwa-Shally02@users.noreply.github.com>
2023-05-22 16:13:57 +0200

    first commit

--- TableCellLesson/ViewController.swift
+++ TableCellLesson/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    
+    @IBOutlet weak var TableView: UITableView!
+    
+    var cars = ["Toyota", "VW", "BMW", "Mercedes", "Haval"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        TableView.dataSource = self
     }
-
+    
+    
+    
 
 }
 
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection Section:
+                   Int) -> Int{
+        
+        return cars.count
+        
+        
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = cars[indexPath.row]
+        return cell
+    }
+    
+  
+    }
+
+
+    
