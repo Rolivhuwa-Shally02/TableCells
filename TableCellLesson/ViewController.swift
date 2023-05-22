65cc9e7a5442fdb68397aa1298884e99fb256e05
Rolivhuwa-Shally02 <80606439+Rolivhuwa-Shally02@users.noreply.github.com>
2023-05-22 15:38:09 +0200

    Initial Commit

---
+++ TableCellLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableCellLesson
+//
+//  Created by DA MAC M1 118 on 2023/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
